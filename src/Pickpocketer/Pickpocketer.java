59a7ab40d8cd03252daf3bfaaf2e997b6a814172
stricklandrbls <strickland.rbls@gmail.com>
2023-10-21 23:14:44 -0500

    Pickpocketing Masterfarmer working.

---
+++ src/Pickpocketer/Pickpocketer.java
@@ -0,0 +1,58 @@
+package Pickpocketer;
+import com.epicbot.api.shared.APIContext;
+import com.epicbot.api.shared.GameType;
+import com.epicbot.api.shared.model.Area;
+import com.epicbot.api.shared.model.Tile;
+import com.epicbot.api.shared.script.LoopScript;
+import com.epicbot.api.shared.script.ScriptManifest;
+import com.epicbot.api.shared.util.paint.frame.PaintFrame;
+import com.epicbot.api.shared.util.paint.frame.Seperator;
+
+import java.awt.*;
+import java.util.Random;
+
+import Pickpocketer.Player;
+
+@ScriptManifest(name = "Pockpicketer", gameType = GameType.OS)
+public class Pickpocketer extends LoopScript {
+    Random rand = new Random();
+    Player player = new Player();
+    String statusMsg = "Initiating";
+
+    @Override
+    public boolean onStart(String... strings) {
+        States.Relocating.setDestination(Constants.FarmingGuildArea, States.Pickpocketing);
+        player.state = States.Organizing;
+        return true;
+    }
+
+    @Override
+    protected int loop() {
+        statusMsg = player.update();
+        moveScreen();
+        return player.state.actionTime();
+    }
+
+    @Override
+    protected void onPaint(Graphics2D g, APIContext ctx) {
+        PaintFrame frame = new PaintFrame("Test Script");
+        frame.addLine("State", statusMsg);
+        frame.draw(g, 0, 170, ctx);
+    }
+
+    protected void moveMouse() {
+        switch(rand.nextInt(10)) {
+
+        }
+    }
+    protected void moveScreen() {
+        switch(rand.nextInt(30)) {
+            case 10:
+                APIContext.get().camera().setPitch(80+rand.nextInt(15));
+                break;
+            case 20:
+                APIContext.get().camera().setYaw(rand.nextInt(2050));
+                break;
+        }
+    }
+}
---
+++ src/Pickpocketer/Player.java
@@ -0,0 +1,24 @@
+package Pickpocketer;
+import com.epicbot.api.shared.APIContext;
+import com.epicbot.api.shared.entity.NPC;
+import com.epicbot.api.shared.methods.ILocalPlayerAPI;
+import com.epicbot.api.shared.model.Tile;
+
+import Pickpocketer.PlayerState;
+
+public class Player {
+
+    protected PlayerState state;
+    protected String statusMsg = "Initialializing";
+
+    public int health() { return APIContext.get().localPlayer().getHealthPercent(); }
+    public boolean inCombat() { return APIContext.get().localPlayer().isInCombat(); }
+    public Tile location() { return APIContext.get().localPlayer().getLocation(); }
+    public String update() { 
+        state.update(this);
+        return state.status();
+    }
+    public boolean isInteractingWith(NPC target) {
+        return APIContext.get().localPlayer().getInteracting() == target;
+    }
+}
